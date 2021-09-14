a14e3864fb773b367c92f9f3149cef502370a29d
TellmeforDWC <rikugame1003@gmail.com>
2021-09-14 18:35:45 +0000

    [finish]Bookersの作成：全機能実装完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:succes] = "Book was succesfully created."
+      flash[:succes] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -24,16 +24,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:succes] = "Book was succesfully updated."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:succes] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    flash[:succes] = "Book was succesfully deleted."
+    flash[:succes] = "Book was successfully deleted."
     redirect_to books_path
   end
 
