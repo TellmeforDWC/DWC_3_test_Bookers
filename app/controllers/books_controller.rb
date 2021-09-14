98e062cc26768fc6a24434792da8268ccf304bf1
TellmeforDWC <rikugame1003@gmail.com>
2021-09-14 13:19:28 +0000

    [update]Bookersの作成：編集機能、削除機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,9 @@
 class BooksController < ApplicationController
   def index
     @book = Book.all
+  end
+
+  def new
     @book = Book.new
   end
 
@@ -8,16 +11,29 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def new
-  end
-
   def create
     book = Book.new(book_params)
     book.save
+    flash[:succes] = "Book was succesfully created."
     redirect_to book_path(book.id)
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:succes] = "Book was succesfully updated."
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:succes] = "Book was succesfully deleted."
+    redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root :to => 'homes#index'
   get 'homes/index'
-  resources :books, :except => :new
+  resources :books, :except => [:new, :destroy]
   post "books" => "books#create", as: "new_book"
+  get "books" => "books#new", as: "create_empty_book"
+  delete "books/:id" => "books#destroy", as: "destroy_book"
 end
